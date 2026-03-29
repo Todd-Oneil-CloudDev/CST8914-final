83bbb19d8106f9b1d09e42f50e24ce0996fa5a7c
Todd-Oneil-CloudDev <onei0240@algonquinlive.com>
2026-03-28 23:06:24 -0400

    created schedule a call page and all required css and javascript for toggle switch

--- EmpowerAbilityLab.js
+++ EmpowerAbilityLab.js
@@ -1,8 +1,33 @@
 //Template Function that can be used to run JavaScript on the page
 //Note: This can be changed to whatever JavaScript formatting you would like
+'use strict';
 function knowledgeRunner(){
 
 }
+
+class CheckboxSwitch {
+  constructor(node) {
+    this.switchNode = node;
+    this.switchNode.addEventListener('focus', () => this.onFocus(event));
+    this.switchNode.addEventListener('blur', () => this.onBlur(event));
+  }
+
+  onFocus(event) {
+    event.currentTarget.parentNode.classList.add('focus');
+  }
+
+  onBlur(event) {
+    event.currentTarget.parentNode.classList.remove('focus');
+  }
+}
+
+// Initialize switches
+window.addEventListener('load', function () {
+  // Initialize the Switch component on all matching DOM nodes
+  Array.from(
+    document.querySelectorAll('input[type=checkbox][role^=switch]')
+  ).forEach((element) => new CheckboxSwitch(element));
+});
 
 
 
